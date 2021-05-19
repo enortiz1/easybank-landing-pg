39f8ed1aca7ed23470c304b7c29dccaf04c26b66
enortiz1 <55902065+enortiz1@users.noreply.github.com>
2021-05-19 18:55:47 -0300

    agrega hero section

--- app/js/script.js
+++ app/js/script.js
@@ -1,4 +1,5 @@
 const btnHamburger = document.querySelector("#btnHamburger");
+const body = document.querySelector("body");
 const header = document.querySelector(".header");
 const overlay = document.querySelector(".overlay");
 const fadeElems = document.querySelectorAll(".has-fade");
@@ -7,6 +8,7 @@ btnHamburger.addEventListener("click", function () {
   console.log("click hamburguer");
 
   if (header.classList.contains("open")) {
+    body.classList.remove('noscroll');
     header.classList.remove("open");
     fadeElems.forEach(function (element) {
       element.classList.remove("fade-in");
@@ -15,6 +17,7 @@ btnHamburger.addEventListener("click", function () {
     
   } else {
     //Open Hamburger Menu
+    body.classList.add('noscroll');
     header.classList.add("open");
     fadeElems.forEach(function (element) {
       element.classList.remove("fade-out");
